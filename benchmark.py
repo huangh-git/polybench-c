7e67a590aa09d43d9793958026e772b96edae61d
huanghan <hh@hhdeMacBook-Pro.local>
2023-10-05 21:14:55 +0800

    get average performance overhead in benchmark.py

--- benchmark.py
+++ benchmark.py
@@ -1,12 +1,14 @@
 import os
 import subprocess
 import argparse
-
-
-SUFFIX = ".wasm"
+import re
 
 def is_wasm(file_path):
-    return file_path.endswith(SUFFIX)
+    return file_path.endswith('.wasm')
+
+testfiles = []
+overheads = []
+tmp_time = 0.0
 
 def execute_and_log(executable, log_file):
     with open(log_file, 'a') as log:
@@ -15,15 +17,25 @@ def execute_and_log(executable, log_file):
         log.write(f'Running {executable}...\n')
         log.write(result.stdout + '\n')
         log.write(result.stderr + '\n')
+        match = re.search(r'\[INFO\] Normalized time: (\d+\.\d+)', result.stdout)
+        if match:
+            normalized_time = float(match.group(1))
+            if is_wasm(executable):
+                global tmp_time
+                tmp_time = normalized_time
+            else:
+                overheads.append((tmp_time - normalized_time)/normalized_time)
+        else:
+            print('No match found in stdout.')
+            exit(1)
 
 def main():
     parser = argparse.ArgumentParser(description='Run all .wasm files in a directory and its subdirectories, and log the output.')
     parser.add_argument('--log', default='output.log', help='Path to the log file.')
-    parser.add_argument('--suffix', default='.wasm', help='Path to the root directory.')
+    # parser.add_argument('--suffix', default='.wasm', help='Path to the root directory.')
     args = parser.parse_args()
 
     log_file = args.log
-    SUFFIX = args.suffix
 
     # Clear or create the log file
     open(log_file, 'w').close()
@@ -34,8 +46,15 @@ def main():
             if is_wasm(file_path):
                 execute_and_log(file_path, log_file)
                 execute_and_log(file_path.replace(".wasm", ".raw-wasm"), log_file)
+                testfiles.append(file_path[:-5])
 
     print(f'Script finished. Output logged to {log_file}')
+    length = len(testfiles)
+    sum_overheads = 0.0
+    for i in range(length):
+        print(f'{testfiles[i]:<55}, {overheads[i]:<10.5f}')
+        sum_overheads += overheads[i]
+    print("average overhead:", sum_overheads/length)
 
 if __name__ == '__main__':
     main()
